9741e253a5f1d792114f08a606f1331eb76f197f
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-07-31 12:48:03 -0300

    Fixed wrong hierarchy

--- account_ddjj_export/models/account_ddjj.py
+++ account_ddjj_export/models/account_ddjj.py
@@ -274,14 +274,14 @@ class DDJJExport:
     def montoComprobante(self,comprobante,tipo_operacion):
             if tipo_operacion == 1:
                 suma_factura = 0
-                if line.full_reconcile_id:
-                    related_movements = self.record.env['account.move.line'].search([
-                    ('full_reconcile_id', '=', line.full_reconcile_id.id)])
-                    credit_sum = sum(line.credit for line in related_movements if line.credit > 0)
-                    # Puedes almacenar o usar 'credit_sum' según sea necesario
-                    suma_factura = credit_sum
-                else:
-                    for line in comprobante.matched_move_line_ids:
+                for line in comprobante.matched_move_line_ids:
+                    if line.full_reconcile_id and suma_factura != 0:
+                        related_movements = self.record.env['account.move.line'].search([
+                        ('full_reconcile_id', '=', line.full_reconcile_id.id)])
+                        credit_sum = sum(line.credit for line in related_movements if line.credit > 0)
+                        # Puedes almacenar o usar 'credit_sum' según sea necesario
+                        suma_factura = credit_sum
+                    elif not line.full_reconcile_id:
                         suma_factura += line.credit
                 return suma_factura
             else:
@@ -419,6 +419,8 @@ class DDJJExport:
                     credit_sum = sum(line.credit for line in related_movements if line.credit > 0)
                     # Puedes almacenar o usar 'credit_sum' según sea necesario
                     suma_factura = credit_sum
+                elif not line.full_reconcile_id:
+                    suma_factura += line.credit
             return suma_factura - base_retencion - monto_retencion
         else:
             return 0
