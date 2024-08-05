290eed73bc43f41538cd1a81cee35ae26ecad595
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-08-05 11:53:20 -0300

    Fix formattign

--- account_ddjj_export/models/account_ddjj.py
+++ account_ddjj_export/models/account_ddjj.py
@@ -197,7 +197,7 @@ class DDJJExport:
             formatted_line += '{:.2f}'.format(self.montoSujetoARetencion(comprobante,56,tipo_operacion)).replace('.','').rjust(12, ' ')
             formatted_line += '{:.2f}'.format(self.porcentajeAlicuota(comprobante,56,tipo_operacion)).replace('.','').rjust(4,'0') #Alicuota
             formatted_line += '{:.2f}'.format(self.montoRetenido(apunte,comprobante,56,tipo_operacion)).replace('.','').rjust(10, ' ')
-            formatted_line += str('  ')
+            formatted_line += str('    ')
             formatted_line += str(self.negativoJujuy(comprobante))
             formatted_line += str('0').ljust(11,'0')
             formatted_lines.append(formatted_line)
