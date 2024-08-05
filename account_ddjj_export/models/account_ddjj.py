9ac4deed7d169715f25a528e6b4d458a7c57a785
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-08-05 11:24:06 -0300

    Fixed typo

--- account_ddjj_export/models/account_ddjj.py
+++ account_ddjj_export/models/account_ddjj.py
@@ -194,10 +194,10 @@ class DDJJExport:
             formatted_line += str('0032').ljust(4,'0')
             formatted_line += str('   ')
             formatted_line += str(comprobante.sequence_number).rjust(8,'0')
-            formatted_line += str(self.negativoJujuy(selg,tipo_operacion))
+            formatted_line += str(self.negativoJujuy(self,tipo_operacion))
             formatted_line += '{:.2f}'.format(self.montoSujetoARetencion(comprobante,56,tipo_operacion)).replace('.','').rjust(12, ' ')
             formatted_line += '{:.2f}'.format(self.porcentajeAlicuota(comprobante,56,tipo_operacion)).replace('.','').rjust(4,'0') #Alicuota
-            formatted_line += str(self.negativoJujuy(selg,tipo_operacion))
+            formatted_line += str(self.negativoJujuy(self,tipo_operacion))
             formatted_line += '{:.2f}'.format(self.montoRetenido(apunte,comprobante,56,tipo_operacion)).replace('.','').rjust(10, ' ')
             formatted_line += str('  ')
             formatted_line += str('0').ljust(11,'0')
