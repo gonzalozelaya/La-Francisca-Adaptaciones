2cac75e49830ecc6cbc9c49bb68f2949a666f33a
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-08-05 17:40:13 -0300

    Fixed some fields

--- account_ddjj_export/models/account_ddjj_settings.py
+++ account_ddjj_export/models/account_ddjj_settings.py
@@ -1,5 +1,17 @@
-class AccountDDJJSettings(models.TransientModel):
+from odoo import models, fields
+
+class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
-   
-    tax_group_id = fields.Many2one('account.tax.group', string='Grupo de Impuestos')
-    account_id = fields.Many2one('account.account', string='Cuenta del Plan de Cuentas')
+
+    tax_group_id = fields.Many2one(
+        'account.tax.group', 
+        string='Grupo de Impuestos',
+        config_parameter='account_ddjj_settings.tax_group_id',
+        help="Seleccione el grupo de impuestos"
+    )
+    account_id = fields.Many2one(
+        'account.account', 
+        string='Cuenta del Plan de Cuentas',
+        config_parameter='account_ddjj_settings.account_id',
+        help="Seleccione la cuenta del plan de cuentas"
+    )
